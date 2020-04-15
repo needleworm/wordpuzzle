bd2c1f3b805460905b47154ed59bd8a02a06bb4f
bhban <needleworm@kaist.ac.kr>
2020-04-15 23:58:30 +0900

    ban_list

--- refine.py
+++ refine.py
@@ -2,24 +2,28 @@ from multiprocessing import Pool, freeze_support
 import sys
 import os
 
-keyword = sys.argv[1]
-numcore = int(sys.argv[2])
+directory = sys.argv[1]
+ban_list = sys.argv[2]
+numcore = int(sys.argv[3])
 
+ban_file = open(ban_list)
+BAN = ban_file.read().split("\n")
+ban_file.close()
 
 def refine_a_file(filename):
     if ".csv" not in filename:
         return False
-    file = open(filename)
+    file = open(directory + "/" + filename)
     rd = file.read()
     rd = rd.split("\n")
     file.close()
-    newfile = open(filename, 'w')
+    newfile = open(directory + "/" + filename, 'w')
     for line in rd:
         try:
             a, b, c = line.strip().split(", ")
         except:
             continue
-        if a == keyword or c == keyword:
+        if a in keyword or c in ban_list:
             continue
         else:
             newfile.write(line)
@@ -33,7 +37,7 @@ if __name__ == "__main__":
     pool = Pool(numcore)
 
     count = 1
-    files = os.listdir()
+    files = os.listdir(directory)
     for result in pool.imap_unordered(refine_a_file, (n for n in files)):
         print(count)
         count += 1
